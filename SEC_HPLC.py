c7375e3ba907047a1aaaf49e38ff8ad0faba030f
David Perez <DPerez36@its.jnj.com>
2024-07-23 10:18:08 +0200

    cleaned file and started calculations

--- SEC_HPLC.py
+++ SEC_HPLC.py
@@ -19,12 +19,14 @@ class sec_HPLCMethod():
         self.gwl_file_name = r"\sample_transfer"
         self.used_labware_pos = {lw: 0 for lw in LabwareNames} # initialize labware positions 
 
+        self.has_detectability_standard = False # only some products have it
+
         # Sample transfer parameters
-        self.n_samples = 0 # amount of samples for the sample transfer
-        self.sample_volume_per_well = 20 # volume (uL) to transfer to each well
-        self.sample_triplicates = False
+        self.n_samples = 1 # amount of samples for the sample transfer
+        self.sample_initial_concentration = 1
+        self.sample_volume_per_well = 1 # volume (uL) to transfer to each well
         self.sample_lw_origin = "" # origin labware of samples
-        self.sample_lw_dest = "384 Well" # destination labware of samples
+        self.sample_lw_dest = "" # destination labware of samples
         self.sample_dest_positions = [0] # positions of 384 plate where the diluted samples end up
 
 
@@ -65,88 +67,57 @@ class sec_HPLCMethod():
                 self.next_labware_pos(LabwareNames[self.sample_lw_origin])
   
 
-
-    def calculate_deep_well_positions(self):
+    def is_sample_dilution_needed(self):
         """
-        Calculate well positions of pos/neg control and samples for the pump labware.
-
+        Calculates if the samples need to be diluted or not.
+        
         Returns
-        --------
-        List containing well positions of samples.
-
-        Example
-        --------
-        >>> self.n_samples = 6
-            calculate_deep_well_positions()
-        [[1, 17, 33], [49, 65, 81], [97, 113, 129], [145, 161, 177], [193, 209, 225], [241, 257, 273]]
-
-        """
-        
-        sample_pos = []
-
-        for sample in [i for i in range(1, self.n_samples+1)]: # create list from 1 to number of samples
-            if self.sample_triplicates:
-                sample_pos.append(get_deep_well_pos(sample, 384, sample_direction="horizontal"))
-            
-            else:
-                sample_pos.append(get_deep_well_pos(sample, 384, sample_direction="horizontal", sample_transfer="single"))
-
-        # final_pos = flatten(sample_pos)
-
-        self.sample_dest_positions = sample_pos # update variable
- 
-        return sample_pos
-
-
-    def generate_gwl_file(self, change_tip:bool = False):
-        """
-        Generates GWL file for the transfer to the 384 well plate.
-        
-        Parameters
         ----------
-        ``change_tip`` : bool
-            True if tips should be changed for every sample, False to just flush tips.
-
+        dict
+            True or False depending if the samples have to be diluted
         """
 
-        # LabDest, DestWell = dilution_position_def("DeepWell", self.next_deep_well_pos(), 1) # define destination labware as deep well
+        # dict to return containing essential data
+        sample_dilution_data = {"sample_dilution_needed": False, # if this is False, the other parameters are useless
+                                  "final_concentration": 0, # in mg/mL
+                                  "injection_volume": 20} # in uL
 
-        output_file_path = self.files_path + self.gwl_file_name + ".gwl"
+        # as in the table in the TMD
+        if self.sample_initial_concentration > 10:
+             new_values = [True, 10, 20]
+        elif self.sample_initial_concentration == 10:
+             new_values = [False, 0, 0]
+        elif self.sample_initial_concentration > 4 and self.sample_initial_concentration < 10:
+             new_values = [True, 4, 50]
+        elif self.sample_initial_concentration > 2 and self.sample_initial_concentration < 4:
+             # special case, needs extra calculations
+             column_load = 0.2 # 200nL, ask nicolas
+             volume = column_load / self.sample_initial_concentration # ???
+             final_concentration = "???" # ask nicolas
+             
+             new_values = [True, final_concentration, volume]
 
-        with open(output_file_path, 'w') as output_file:
-            # Read all lines from the input file
 
-            output_lines = []
-            
-            
-            LabSource, SourceWell = dilution_position_def(self.sample_lw_origin, 1, self.n_samples)
-            # Extract the values
-            for sample in [i for i in range(self.n_samples)]: # create list from 1 to number of samples
-                LabDest, _ = dilution_position_def(self.sample_lw_dest, 1, 1) # ignore received position as we already have it in self.sample_dest_positions
-                DestWell = self.sample_dest_positions[sample]
-                Volume = self.sample_volume_per_well
-                
-                # Create the three lines for the output
-                if self.sample_triplicates:
-                    a_line = f"A;{LabSource[sample]};;;{SourceWell[sample]};;{Volume*3};;;;\n" # 3x volume to save time
-                    d_line_1 = f"D;{LabDest[0]};;;{DestWell[0]};;{Volume};;;;\n"
-                    d_line_2 = f"D;{LabDest[0]};;;{DestWell[1]};;{Volume};;;;\n"
-                    d_line_3 = f"D;{LabDest[0]};;;{DestWell[2]};;{Volume};;;;\n"
-                else:
-                    a_line = f"A;{LabSource[sample]};;;{SourceWell[sample]};;{Volume};;;;\n"
-                    d_line_1 = f"D;{LabDest[0]};;;{DestWell};;{Volume};;;;\n"
+        elif self.sample_initial_concentration == 2:
+             new_values = [False, 2, 100]
+        else:
+            raise ValueError("The sample initial concentration needs to be at least 2mg/mL")
 
-                w_line = "W;\n" if change_tip else "F;\n" # use F to flush tip remaining contents, W to change tips instead of changing tip
-                
-                # Append the lines to the output lines list
-                output_lines.append(a_line)
-                output_lines.append(d_line_1)
-                if self.sample_triplicates: # if triplicates do 2 more
-                    output_lines.append(d_line_2)
-                    output_lines.append(d_line_3)
-                output_lines.append(w_line)
+        
+        sample_dilution_data = {k: v for k, v in zip(sample_dilution_data.keys(), new_values)}
+        
 
-            output_file.writelines(output_lines)
+        return sample_dilution_data
+
+
+    def sample_dilution(self, sample_dilution_data):
+        """
+        Only called if samples need to be diluted. Generates the CSV files for the dilution.
+        """
+
+        
+
+        pass
 
 
     def sec_HPLC(self):
@@ -159,11 +130,11 @@ class sec_HPLCMethod():
 
         self.count_starting_lw_pos()
 
-        self.calculate_deep_well_positions()
-        print("Calculated 384 well positions:", self.sample_dest_positions)
+        sample_dilution_data = self.is_sample_dilution_needed()
+        print("Sample dilution data:", sample_dilution_data)
 
-        self.generate_gwl_file()
-        print("Generated GWL file.")
+
+
 
         return
 
@@ -182,8 +153,8 @@ class sec_HPLCMethod():
         self.used_labware_pos = dict.fromkeys(self.used_labware_pos, 0) # reset dict
 
         # General
-        self.n_samples = external.nDSF_n_samples.get() # amount of samples for the sample transfer
-        self.sample_volume_per_well = external.nDSF_volume.get() # volume (uL) to transfer to each well
-        self.sample_lw_origin = external.nDSF_lw_origin.get() # origin labware of samples
-        self.sample_triplicates = True if external.nDSF_sample_triplicates.get() == "Triplicate transfer" else False
+        self.n_samples = external.sec_HPLC_n_samples.get() # amount of samples for the sample transfer
+        self.sample_lw_origin = external.sec_HPLC_lw_origin.get() # origin labware of samples
+        self.sample_initial_concentration = int(external.sec_HPLC_initial_concentration.get()) # origin labware of samples
+        self.sample_lw_dest = external.sec_HPLC_lw_dest.get() # origin labware of samples
         
