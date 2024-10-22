e0e4bd9ae2044c5ab72584b28c7e492f75df8ef6
David Perez <DPerez36@its.jnj.com>
2024-10-22 08:36:00 +0200

    first working version DPP

--- helper/DrugProductPreparation.py
+++ helper/DrugProductPreparation.py
@@ -17,6 +17,7 @@ class DrugProductPreparationMethod():
 
         # General parameters
         self.csv_files_path = r'L:\Departements\BTDS_AD\002_AFFS\Lab Automation\09. Tecan\01. Methods\11. Drug Product Preparation' # network path where all the CSV files will be saved in.
+        self.csv_filename = r"\dpp_step - "
         self.config_file_name = r"\config.txt"
         self.used_labware_pos = {lw: 0 for lw in LabwareNames} # initialize labware positions
         self.csv_number = 1 # to keep track of generated CSV files
@@ -24,10 +25,11 @@ class DrugProductPreparationMethod():
         # Sample transfer parameters
         self.n_samples = 1 # amount of samples for the sample transfer
         self.sample_initial_concentration = 1
-        self.sample_final_concentration = 1 # mg/mL, it is always like this
-        self.sample_volume_per_well = 1 # volume (uL) to transfer to each well
+        self.final_concentration = 1 # mg/mL, it is always like this
+        self.sample_volume_transfer = 1 # volume (uL) to transfer to each well
         self.sample_lw_origin = "" # origin labware of samples
-        self.lw_dest = LabwareNames["Eppendorf"]
+        self.lw_dest = "Eppendorf"
+        self.total_volume = 1000 # total volume to have in destination labware
 
         # Labware names
         self.buffer_lw_origin = LabwareNames["GeneralBuffer"] # origin labware of buffer, hard coded for now
@@ -70,105 +72,75 @@ class DrugProductPreparationMethod():
 
         if self.sample_lw_origin in LabwareNames:
             for i in range(0, self.n_samples):
-                self.next_labware_pos(LabwareNames[self.sample_lw_origin])
+                self.next_labware_pos(self.sample_lw_origin)
   
 
     def sample_dilutions(self):
         """
         Performs the dilutions to get the sample final concentration.
 
-        Parameters
-        ----------
-        ``initial_sample_transfer``: int
-            At the beginning, transfer once this amount of volume (uL) of the samples to the wells, so that for later steps,
-            all the volumes of the samples can be taken from here with smaller tips rather than changing tip all the time.
-
         Outputs
         ----------
             CSV files containing instructions for the Tecan.
         """
 
-        csv_data_init = []
-        csv_number = 0 # # to name generated files sequentially
-        dest_positions = [] # list to store destination positions used for the final dilution
+        csv_data_sample = []
+        csv_data_buffer = []
+
+        sample_volume, buffer_volume = calculate_dilution_parameter(self.sample_initial_concentration, self.final_concentration, None, self.total_volume)
+
+        LabDest, DestWell = dilution_position_def(self.lw_dest, self.next_labware_pos(self.lw_dest), 1)
+
+        # sample to dest labware
+        LabSource, SourceWell = dilution_position_def(LabwareNames[self.sample_lw_origin], 1, self.n_samples) # samples are always placed in positions 1..n_samples
+        csv_data_sample.append(
+        {
+            'LabSource': LabSource[0],
+            'SourceWell': SourceWell[0],
+            'LabDest': LabDest[0],
+            'DestWell': DestWell[0],
+            'Volume': sample_volume
+        })
         
-        LabSource, SourceWell = self.dilution_position_def(self.sample_lw_origin, 1, self.n_samples)
-        LabDest, DestWell = self.dilution_position_def(self.sample_int_dil_destination, self.used_labware_pos["DeepWell"] + 1, self.n_samples)
-
-            # Initial transfer of sample to deep wells, to do intermediate dilutions
-        if self.samples_initial_volume_transfer != 0:
-            for j in range(0, self.n_samples):
-
-                # LabSource = self.pos_2_str(self.sample_lw_origin, j+1)
-                csv_data_init.append(
-                {
-                    'LabSource': LabSource[j],
-                    'SourceWell': SourceWell[j],
-                    'LabDest': LabDest[j],
-                    'DestWell': DestWell[j],
-                    'Volume': float(self.samples_initial_volume_transfer)
-                }
-                )
-                self.next_labware_pos("DeepWell") # to keep the position updated
-                
-            path = self.csv_files_path + self.sample_dilutions_csv_name + str(csv_number) + ".csv"
-            pd.DataFrame(csv_data_init).to_csv(path, index=False, header=False)
-            csv_number = csv_number + 1
-        
-        for i in range(self.n_sample_dilution_steps):
-            csv_data_sample = []
-            csv_data_buffer = []
-
-            LabSource, SourceWell = LabDest, DestWell # source of next step is destination of previous one
-
-            if i + 1 == self.n_sample_dilution_steps: # if this is the final dilution step
-                LabDest, DestWell = self.dilution_position_def(self.sample_lw_dest, self.used_labware_pos[self.sample_lw_dest] + 1, (self.n_samples * (i+1) + 1))
-            else:
-                LabDest, DestWell = self.dilution_position_def("DeepWell", self.used_labware_pos["DeepWell"] + 1, (self.n_samples * (i+1) + 1))
-
-            for j in range(self.n_samples):
-                csv_data_sample.append(
-                {
-                    'LabSource': LabSource[j],
-                    'SourceWell': SourceWell[j],
-                    'LabDest': LabDest[j],
-                    'DestWell': DestWell[j],
-                    'Volume': float(self.sample_dilution_data["Sample volume"][i])
-                }
-                )
-                
-                csv_data_buffer.append(
-                {
-                    'LabSource': utils.LabwareNames["AssayBuffer"],
-                    'SourceWell': int(1),
-                    'LabDest': LabDest[j],
-                    'DestWell':  DestWell[j],
-                    'Volume': float(self.sample_dilution_data["Assay buffer volume"][i])
-                }
-                )
-                if i + 1 == self.n_sample_dilution_steps: # if this is the last dilution step
-                    self.next_labware_pos(self.sample_lw_dest)
-                    dest_positions.append(self.used_labware_pos[self.sample_lw_dest])
-                else:
-                    self.next_labware_pos("DeepWell")
-
-            path = self.csv_files_path + self.sample_dilutions_csv_name + str(csv_number) + ".csv"
-            pd.DataFrame(csv_data_sample).to_csv(path, index=False, header=False)
-            path = self.csv_files_path + self.sample_dilutions_csv_name + str(csv_number + 1) + ".csv"
-            pd.DataFrame(csv_data_buffer).to_csv(path, index=False, header=False)
-            csv_number = csv_number + 2
-
-        # if less than 3 dilutions steps are needed, blank out the remaining CSV files so that the Tecan ignores them basically
-        for i in range(csv_number, 6 + 1):
-            path = self.csv_files_path + self.sample_dilutions_csv_name + str(csv_number) + ".csv"
-            pd.DataFrame(list()).to_csv(path, index=False, header=False) # create empty dataframe and save it into an empty CSV
-            csv_number = csv_number + 1
+        # buffer to dest labware
+        csv_data_buffer.append(
+        {
+            'LabSource': self.buffer_lw_origin,
+            'SourceWell': 1,
+            'LabDest': LabDest[0],
+            'DestWell': DestWell[0],
+            'Volume': buffer_volume
+        })
 
 
-        return dest_positions
+        path = self.csv_files_path + self.csv_filename + str(self.csv_number) + ".csv"
+        pd.DataFrame(csv_data_sample).to_csv(path, index=False, header=False)
+        path = self.csv_files_path + self.csv_filename + str(self.csv_number + 1) + ".csv"
+        pd.DataFrame(csv_data_buffer).to_csv(path, index=False, header=False)
+        self.csv_number += 2
+
+    
+    def generate_config_file(self):
+        """
+        Generates the config file for the current run.
+        """
+
+        # If there are repeated keys in the dictionary, the last one and its value is the dominant one !!!
+
+        config_parameters = {"n_steps": self.csv_number - 1 # we remove 1 because it is already added beforehand
+                     }
+
+        with open(self.csv_files_path + self.config_file_name, 'w') as file:
+            # Write the keys
+            keys = "; ".join(config_parameters.keys())
+            file.write(keys + ";\n")
+            
+            # Write the values
+            values = "; ".join(map(str, config_parameters.values()))
+            file.write(values + ";\n")
     
     
-    def general_dilution(self):
+    def DrugProductPreparation(self):
         """
         Class main method.
         ----------
@@ -178,12 +150,24 @@ class DrugProductPreparationMethod():
 
         # Reset parameters
         self.used_labware_pos = dict.fromkeys(self.used_labware_pos, 0)
+        self.csv_number = 1
 
+        logger.info("-------------------------------------")
+        logger.info(f"Samples initial labware: {self.sample_lw_origin}")
+        logger.info(f"Samples destination labware: {self.lw_dest}")
+        logger.info(f"Samples initial concentration: {self.sample_initial_concentration} mg/mL")
+        logger.info(f"Final concentration: {self.final_concentration} mg/mL")
+        logger.info("-------------------------------------")
+        
         self.count_starting_lw_pos()
 
-        self.sample_dest_positions = self.sample_dilutions()
+        self.sample_dilutions()
+        logger.info(f"Dilution CSV files generated.")
 
-        return self.sample_dest_positions
+        self.generate_config_file()
+        logger.info("Config file generated.")
+        
+        logger.info(f"Method finished successfully.")
 
         
     def set_all_parameters(self, external):
@@ -197,16 +181,10 @@ class DrugProductPreparationMethod():
         """
         # Reset parameters
         self.used_labware_pos = dict.fromkeys(self.used_labware_pos, 0)
-
-        # General
-        self.sample_dilution_data = external.sample_dilution_data
-        self.n_sample_dilution_steps = len(self.sample_dilution_data["Assay buffer volume"])
+        self.csv_number = 1
 
         # Sample
-        self.sample_lw_origin = external.optionmenu_1_gd.get()
-        self.n_samples = int(external.entry_slider2_gd.get())
-        self.buffer_type = str(external.optionmenu_buffer1_gd.get())
-        self.sample_lw_dest = external.gd_dil_dest.get()
+        self.sample_lw_origin = external.drug_prod_prep_sample_lw_origin.get()
+        self.lw_dest = external.drug_prod_prep_lw_dest.get()
         
-        # self.samples_initial_volume_transfer = external.entry_slider3.get()
 
