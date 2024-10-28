82567796cc32e586725395fad99a7d86d29598df
David Perez <DPerez36@its.jnj.com>
2024-10-28 15:04:22 +0100

    updated method to use logfile and fixed main calculations

--- helper/A280.py
+++ helper/A280.py
@@ -10,7 +10,7 @@ from helper.utils import * # file with helper methods
 
 class A280Method():
     """
-    Produces CSV files with all the steps to carry out the A280 method in the TECAN.
+    Produces CSV files with all the steps to carry out the A280 method dilutions and transfer in the TECAN.
     """
 
     def __init__(self):
@@ -27,8 +27,15 @@ class A280Method():
         self.sample_lw_origin = "" # origin labware of samples
         self.sample_lw_dest = "soloVPE cuvettes" # destination labware of samples
         self.sample_diluted_positions = [0] # positions where the diluted samples end up
+        self.is_dilution_needed = False
+        self.sample_transfer_volume = 100 # volume in uL to transfer to each cuvette
 
-        self.buffer_lw_pos = ("buffer lw", "x") # buffer labware and position if dilution is needed
+        self.buffer_lw = LabwareNames["GeneralBuffer"]
+        self.dilution_extra_volume = 100 # extra volume in uL to assure that tip will have enough volume to take
+        self.dilution_labware = "FakeFalcon15"
+        self.dilution_pos = 0
+
+        self.config_file_name = r"\config.txt"
 
 
     def next_labware_pos(self, labware_name:str):
@@ -64,9 +71,7 @@ class A280Method():
         """
 
         if self.sample_lw_origin in LabwareNames:
-            for i in range(0, self.n_samples):
-                self.next_labware_pos(LabwareNames[self.sample_lw_origin])
-  
+            self.next_labware_pos(LabwareNames[self.sample_lw_origin])
 
 
     def sample_dilution(self):
@@ -79,46 +84,42 @@ class A280Method():
         csv_data_sample = []
         csv_data_buffer = []
 
-        sample_volume_to_transfer = 125 # uL
-        buffer_volume_per_sample = 375 # uL
-
-        buffer_total_volume = buffer_volume_per_sample * self.n_samples
-        print("buffer total volume", buffer_total_volume)
-        # calculate buffer labware depending in the volume used
+        total_volume = self.n_samples * 100 + self.dilution_extra_volume # total diluted volume needed for the transfer to the cuvettes
+        sample_volume, buffer_volume = calculate_dilution_parameter(self.sample_concentration, self.sample_concentration / 4, None, total_volume)
 
         # buffer_lw, buffer_pos = dilution_position_def("Eppendorf", self.next_labware_pos("Eppendorf"), 1) # buffer is placed in Eppendorf tube
-        initial_pos = self.next_labware_pos(find_best_container(buffer_total_volume/1000))
-        print("initial pos:", initial_pos)
-        buffer_lw, buffer_pos = dilution_position_def(find_best_container(buffer_total_volume/1000), initial_pos, 1) # buffer is placed in Eppendorf tube
-        self.buffer_lw_pos = (buffer_lw, buffer_pos)
+        # initial_pos = self.next_labware_pos(find_best_container(buffer_total_volume/1000))
+        # print("initial pos:", initial_pos)
+        # buffer_lw, buffer_pos = dilution_position_def(find_best_container(buffer_total_volume/1000), initial_pos, 1) # buffer is placed in Eppendorf tube
+        # self.buffer_lw_pos = (buffer_lw, buffer_pos)
 
-        print(f"buffer_lw and pos: {buffer_lw}, {buffer_pos}")
+        # print(f"buffer_lw and pos: {buffer_lw}, {buffer_pos}")
 
-        LabSource, SourceWell = dilution_position_def(self.sample_lw_origin, 1, self.n_samples) # samples are always placed in positions 1..n_samples
-        LabDest, DestWell = dilution_position_def("Eppendorf", self.used_labware_pos["Eppendorf"] + 1, self.n_samples)
+        LabSource, SourceWell = dilution_position_def(self.sample_lw_origin, 1, 1) # samples are always placed in positions 1..n_samples
+        LabDest, DestWell = dilution_position_def(self.dilution_labware, self.next_labware_pos(LabwareNames[self.dilution_labware]), 1)
 
         for j in range(self.n_samples):
             csv_data_sample.append(
             {
-                'LabSource': LabSource[j],
-                'SourceWell': SourceWell[j],
-                'LabDest': LabDest[j],
-                'DestWell': DestWell[j],
-                'Volume': sample_volume_to_transfer
+                'LabSource': LabSource[0],
+                'SourceWell': 1,
+                'LabDest': LabDest[0],
+                'DestWell': DestWell[0],
+                'Volume': sample_volume
             }
             )
             
             csv_data_buffer.append(
             {
-                'LabSource': buffer_lw[0],
-                'SourceWell': buffer_pos[0],
-                'LabDest': LabDest[j],
-                'DestWell':  DestWell[j],
-                'Volume': buffer_volume_per_sample
+                'LabSource': self.buffer_lw,
+                'SourceWell': 1,
+                'LabDest': LabDest[0],
+                'DestWell':  DestWell[0],
+                'Volume': buffer_volume
             }
             )
 
-            self.next_labware_pos("Eppendorf") # to keep track of used labware positions
+            # self.next_labware_pos("Eppendorf") # to keep track of used labware positions
 
         path = self.csv_files_path + self.sample_dilution_file_name + str(csv_number) + ".csv"
         pd.DataFrame(csv_data_sample).to_csv(path, index=False, header=False)
@@ -126,7 +127,7 @@ class A280Method():
         pd.DataFrame(csv_data_buffer).to_csv(path, index=False, header=False)
         csv_number = csv_number + 2
 
-        return DestWell
+        return (LabDest, DestWell)
     
 
 
@@ -136,33 +137,27 @@ class A280Method():
         """
 
         
-        if self.sample_concentration >= self.concentration_limit:
-            self.sample_lw_origin = "Eppendorf"
-            LabSource, SourceWell = dilution_position_def(self.sample_lw_origin, self.used_labware_pos[self.sample_lw_origin] - (self.n_samples - 1), self.n_samples) # samples are always placed in positions 1..n_samples
+        if self.is_dilution_needed:
+            LabSource, SourceWell = self.dilution_labware, self.dilution_pos # samples are always placed in positions 1..n_samples
         
         else:
-            LabSource, SourceWell = dilution_position_def(self.sample_lw_origin, 1, self.n_samples) # samples are always placed in positions 1..n_samples
-        print("labsource sample transfer:", LabSource)
+            LabSource, SourceWell = dilution_position_def(LabwareNames[self.sample_lw_origin], 1, 1) # samples are always placed in positions 1..n_samples
 
         csv_number = 1 # # to name generated files sequentially
         csv_data_sample = []
-
-        sample_volume_to_transfer = 100 # uL
 
         LabDest, DestWell = dilution_position_def(self.sample_lw_dest, 1, self.n_samples) # position starts in 1 because labware has not been used yet
 
         for j in range(self.n_samples):
             csv_data_sample.append(
             {
-                'LabSource': LabSource[j],
-                'SourceWell': SourceWell[j],
+                'LabSource': LabSource[0],
+                'SourceWell': SourceWell[0],
                 'LabDest': LabDest[j],
                 'DestWell': DestWell[j],
-                'Volume': sample_volume_to_transfer
+                'Volume': self.sample_transfer_volume
             }
             )
-
-            self.next_labware_pos(self.sample_lw_origin) # to keep track of used labware positions
 
         path = self.csv_files_path + self.sample_transfer_file_name + str(csv_number) + ".csv"
         pd.DataFrame(csv_data_sample).to_csv(path, index=False, header=False)
@@ -170,6 +165,24 @@ class A280Method():
         return DestWell
 
 
+    def generate_config_file(self):
+        """
+        Generates the config file for the current run.
+        """
+
+        # If there are repeated keys in the dictionary, the last one and its value is the dominant one !!!
+
+        config_parameters = {"is_dilution_needed": self.is_dilution_needed # we remove 1 because it is already added beforehand
+                     }
+
+        with open(self.csv_files_path + self.config_file_name, 'w') as file:
+            # Write the keys
+            keys = ";".join(config_parameters.keys())
+            file.write(keys + ";\n")
+            
+            # Write the values
+            values = ";".join(map(str, config_parameters.values()))
+            file.write(values + ";\n")
 
 
     def a280(self):
@@ -180,18 +193,33 @@ class A280Method():
         Executes all stages of the A280 method step by step and generates CSV files for them.
         """
 
+        # Reset parameters
+        self.used_labware_pos = dict.fromkeys(self.used_labware_pos, 0) # reset dict
+        
+        if self.sample_concentration >= self.concentration_limit:
+            self.is_dilution_needed = True # update flag
+
+        logger.info("-------------------------------------")
+        logger.info(f"N. of samples: {self.n_samples}")
+        logger.info(f"Samples initial labware: {self.sample_lw_origin}")
+        logger.info(f"Samples initial concentration: {self.sample_concentration} mg/mL")
+        logger.info(f"Sample dilution needed: {str(self.is_dilution_needed)}")
+        logger.info("-------------------------------------")
+
         self.count_starting_lw_pos()
 
-        if self.sample_concentration >= self.concentration_limit:
-            self.sample_diluted_positions = self.sample_dilution()
-            print("Sample dilutions done. Positions in Eppendorf:", self.sample_diluted_positions)
-            print(f"Buffer labware and position: {self.buffer_lw_pos[0][0]}, {self.buffer_lw_pos[1]}")
+        if self.is_dilution_needed:
+            _, self.dilution_pos = self.sample_dilution()
+            logger.info(f"Sample dilution done.")
 
         dest_positions = self.sample_transfer()
         print("Sample transfer done. Destination positions:", dest_positions)
+        logger.info(f"Sample transfer done. Destination positions: {dest_positions}")
 
-
-        return 0
+        self.generate_config_file()
+        logger.info("Config file generated.")
+        
+        logger.info(f"Method finished successfully.")
 
         
     def set_all_parameters(self, external):
@@ -209,5 +237,5 @@ class A280Method():
 
         # General
         self.n_samples = external.a280_n_samples.get() # amount of samples for the sample transfer
-        self.sample_concentration = int(external.a280_concentration.get()) # volume (uL) to transfer to each well
+        self.sample_concentration = float(external.a280_concentration.get()) # volume (uL) to transfer to each well
         self.sample_lw_origin = external.a280_lw_origin.get() # origin labware of samples
